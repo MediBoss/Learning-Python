032587c9322accd5de9157057d3db436170bd9b5
Medi Assumani <mediassumani49@gmail.com>
2018-05-03 20:37:52 -0400

    manipulation on strings

---
+++ string_manipulation.py
@@ -0,0 +1,12 @@
+# Manipulating strings
+
+first_name = "Medi"
+last_name = "Assumani"
+full_name = first_name + " " + last_name
+
+print "Your first name is : "+ first_name
+print "Your last name is : "+ last_name
+print "Your full name is : "+ full_name
+
+print last_name[0:4] # prints Assu
+print first_name[0] # prints M
