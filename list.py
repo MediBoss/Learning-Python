a033ec462031697e51c76ab1503d84e0153ebe94
Medi Assumani <mediassumani49@gmail.com>
2018-05-03 20:56:28 -0400

    changed name

--- list.py
+++
@@ -1,5 +0,0 @@
-# List in python can contain different types
-
-studend_id = [1989,9087,6754,7894]
-
-print studend_id[0]
---
+++ scrap.py
@@ -0,0 +1,14 @@
+# List in python can contain different types
+
+student_id = [1989,9087,6754,7894,7854,1235,9070]
+
+print student_id[0]
+print student_id[4]
+
+#    Tuples
+
+best_student = ("Medi Assumani",4.0) #tiny tuple here
+worst_student = ("Maceline Ambroise", 1.2)
+
+
+#           Dictionaries
