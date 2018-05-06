fd16ba71c1f75b5cde84bc1c11c26f9d4dfadf75
Medi Assumani <mediassumani49@gmail.com>
2018-05-06 18:15:23 -0400

    working on challenge

--- matrix.py
+++ matrix.py
@@ -4,17 +4,27 @@
         # highest score, lowest score, average
         # Find highest score in Science, English, History, Arts, and Maths
 
-random_students = [['Roy',80,75,85,90,95],
-                   ['John',75,80,75,85,100],
-                   ['Dave',80,80,80,90,95],
-                   ['Medi',90,45,78,85,66],
-                   ['Mace',67,89,77,90,45],
-                   ['Patrcik',97,45,89,65,40]]
+seniors = [['Roy',80,75,85,90,95],
+           ['John',75,80,75,85,100],
+           ['Dave',80,80,80,90,95],
+           ['Medi',90,45,78,85,66],
+           ['Mace',67,89,77,90,45],
+           ['Patrcik',97,45,89,65,40]]
 
-def find_student_average():
+def find_student_average(student_name):
+    average = 0.0
+    for row in seniors:
+        if seniors[row] == student_name:
+            row = row + 1
+            average += seniors[row]
+        else:
+            continue
+        for col in seniors
 
 
-def find_student_lowest_score():
-def find_student_highest_score():
-def find_best_overall_performer():
-def get_student_name():
+
+#def find_student_lowest_score():
+#def find_student_highest_score():
+#def find_best_overall_performer():
+#def find_overall_average()
+# def get_student_name():
