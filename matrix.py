0aff64797ec965d13475a5f5d54799938d6bda5a
Medi Assumani <mediassumani49@gmail.com>
2018-05-06 17:51:57 -0400

    setting up exercise

---
+++ matrix.py
@@ -0,0 +1,20 @@
+# Learing 2D arrays(Matrix) in python
+# Programmer : Medi Assumani
+# Task get each student :
+        # highest score, lowest score, average
+        # Find highest score in Science, English, History, Arts, and Maths
+
+random_students = [['Roy',80,75,85,90,95],
+                   ['John',75,80,75,85,100],
+                   ['Dave',80,80,80,90,95],
+                   ['Medi',90,45,78,85,66],
+                   ['Mace',67,89,77,90,45],
+                   ['Patrcik',97,45,89,65,40]]
+
+def find_student_average():
+
+
+def find_student_lowest_score():
+def find_student_highest_score():
+def find_best_overall_performer():
+def get_student_name():
