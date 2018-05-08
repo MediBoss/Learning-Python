ce8bf5b8c9ede7f0ab102058fce36c3e36228017
Medi Assumani <mediassumani49@gmail.com>
2018-05-07 23:25:44 -0400

    conditional list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,6 +1,9 @@
 # Function to return all letters of a string in a list using list comprehension
 def list_comprehension(word):
     return [letter for letter in word]
+# Function to fillter all test score grater than 80
+def filter_score(list):
+    return [score for score in list if score > 80]
 
 # Function to return all letters of a string in a list using iteration
 def iteration(word):
@@ -10,5 +13,13 @@ def iteration(word):
 
     return word_letters
 
-print iteration('Programming')
-print list_comprehension('Programming')
+    # Organize the codes in the main function
+def main():
+    #print list_comprehension('Medi')
+    #print iteration('Medi')
+
+    test_scores = [89,45,98,67,90,81,80,83,94,95,87]
+    print filter_score(test_scores)
+
+if __name__ == "__main__":
+    main()
