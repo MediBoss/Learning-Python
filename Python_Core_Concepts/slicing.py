65c9507db6453368ebb5f6beddb57a6ed1adf2b3
Medi Assumani <mediassumani49@gmail.com>
2018-11-12 06:59:44 -0800

    Update slicing.py

--- Python_Core_Concepts/slicing.py
+++ Python_Core_Concepts/slicing.py
@@ -1,1 +1,14 @@
 
+""" 
+Slicing helps us to get sub elements of an iterable object without crazy loops
+"""
+    list_one = [1,2,3,4,5,6,7,8,9,10]
+
+    answer_one = list_one[1:4] # gets elements from 1st to 5th index exclusive
+
+    # gets all even numbers from 1st to 8th elementself.
+    # :2 tells python to increment by 2
+    answer_two = list_one[1:7:2]
+
+    # get list backward
+    backward_list = list_one[::-1]
