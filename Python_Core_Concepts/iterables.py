134c496e3a76aa8a0e41562b82832b3a07a9e07f
Medi Assumani <mediassumani49@gmail.com>
2018-11-20 21:17:09 -0800

    Update iterables.py

--- Python_Core_Concepts/iterables.py
+++ Python_Core_Concepts/iterables.py
@@ -1,1 +1,28 @@
 
+import itertools
+import operator
+
+
+def main():
+
+    data = {
+        "b": 3,
+        "a": 2,
+        "c": 2
+    }
+
+    # The accumulate function combines an iterable and function
+
+    # mul : multiplies each element to the previous
+    result_acc = itertools.accumulate(data, operator.mul)
+    for each in result_acc:
+        print(each)
+
+    result_max = itertools.accumulate(data.values(), max)
+    for each in result_max:
+        print(each)
+
+
+
+if __name__ == "__main__":
+    main()
